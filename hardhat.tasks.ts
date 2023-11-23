84342382c04890f1b5b69d6cf5d6c346ff3398ec
0xRektora <13756173+0xRektora@users.noreply.github.com>
2023-11-23 10:40:03 -0500

    fix: typo

--- hardhat.tasks.ts
+++ hardhat.tasks.ts
@@ -2,4 +2,4 @@ import '@nomiclabs/hardhat-ethers';
 import { task } from 'hardhat/config';
 import { deployYieldBox__task } from './tasks/deploy/deployYieldBox';
 
-task('deployYieldbox', 'Deploy YieldBox', deployYieldBox__task);
+task('deployYieldBox', 'Deploy YieldBox', deployYieldBox__task);
